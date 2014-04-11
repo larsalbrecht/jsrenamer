14315cb750f09d60d8a15e2093ddfdfb448971d7
lars.albrecht@gmail.com <lars.albrecht@gmail.com@531251de-4140-9ac6-895e-f70ce99057a9>
2014-04-11 13:29:43 +0000

    Added some tests

--- src/com/lars_albrecht/java/jsrenamer/test/MagicTest.java
+++ src/com/lars_albrecht/java/jsrenamer/test/MagicTest.java
@@ -14,26 +14,14 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class MagicTest {
 
-	public static int getHammingDistance(final String sequence1, final String sequence2) {
-		final char[] s1 = sequence1.toCharArray();
-		final char[] s2 = sequence2.toCharArray();
-
-		final int shorter = Math.min(s1.length, s2.length);
-		final int longest = Math.max(s1.length, s2.length);
-
-		int result = 0;
-		for (int i = 0; i < shorter; i++) {
-			if (s1[i] != s2[i]) {
-				result++;
-			}
-		}
-
-		result += longest - shorter;
-
-		return result;
+	public static void main(final String[] args) {
+		new MagicTest();
 	}
 
-	private static ArrayList<String> getSubparts(final String[] tempArr) {
+	private static ArrayList<String> step1(final String[] tempArr) {
+		final ConcurrentHashMap<Integer, String> staticStrings = new ConcurrentHashMap<Integer, String>();
+		final ConcurrentHashMap<String, ConcurrentHashMap<Integer, String>> variableStrings = new ConcurrentHashMap<String, ConcurrentHashMap<Integer, String>>();
+
 		final ArrayList<String> tempList = new ArrayList<String>();
 
 		// remove "other"
@@ -46,10 +34,28 @@ public class MagicTest {
 				}
 			}
 			if (stringCheck) {
-				System.out.println("ADD");
 				tempList.add(string);
 			}
 		}
+
+		Tupel<ConcurrentHashMap<Integer, String>, ConcurrentHashMap<Integer, String>> tempTupel = null;
+
+		for (int i = 0; i < tempList.size(); i++) {
+			for (int j = 0; j < tempList.size(); j++) {
+				if (!tempList.get(i).equals(tempList.get(j))) {
+					tempTupel = MagicTest.step1Helper(tempList.get(i), tempList.get(j), 0);
+					staticStrings.putAll(tempTupel.getObjectA());
+					variableStrings.put(tempList.get(i), tempTupel.getObjectB());
+				}
+			}
+		}
+
+		System.out.println(staticStrings);
+		System.out.println(variableStrings);
+		// for (Entry<String, Map> entry : variableStrings) {
+		// entry.getValue()
+		// }
+
 		/*
 		 * diff = StringUtils.difference(startString, testString); startDiff =
 		 * testString.indexOf(diff); matches.put(startDiff,
@@ -59,97 +65,59 @@ public class MagicTest {
 		return tempList;
 	}
 
-	public static void main(final String[] args) {
-		new MagicTest();
-	}
+	private static Tupel<ConcurrentHashMap<Integer, String>, ConcurrentHashMap<Integer, String>> step1Helper(final String a,
+			final String b,
+			int position) {
+		final ConcurrentHashMap<Integer, String> staticStrings = new ConcurrentHashMap<Integer, String>();
+		final ConcurrentHashMap<Integer, String> variableStrings = new ConcurrentHashMap<Integer, String>();
 
-	/**
-	 * @see "http://stackoverflow.com/questions/3976616/how-to-find-nth-occurrence-of-character-in-a-string"
-	 * 
-	 * @param string
-	 * @param toFind
-	 * @param n
-	 * @return
-	 */
-	public static int nthOccurrence(final String string, final String toFind, int n) {
-		n--;
-		int pos = string.indexOf(toFind, 0);
-		while ((n-- > 0) && (pos != -1)) {
-			pos = string.indexOf(toFind, pos + 1);
+		// get static
+		String stringDiffA = StringUtils.difference(a, b);
+		String stringDiffB = StringUtils.difference(b, a);
+		final String staticString = b.substring(0, b.indexOf(stringDiffA));
+		staticStrings.put(position, staticString);
+		// search variable
+		boolean foundMore = false;
+		// for (int i = 1; i < (stringDiffA.length() - 1); i++) {
+		int i = 1;
+		while (i < (stringDiffA.length() - 1)) {
+			if (i < (stringDiffA.length() - 1)) {
+				break;
+			}
+			stringDiffA = stringDiffA.substring(i);
+			stringDiffB = stringDiffB.substring(i);
+			final String diffTestA = StringUtils.difference(stringDiffA, stringDiffB);
+			if (stringDiffB.indexOf(diffTestA) > 0) {
+				System.out.println("\t\tput: " + stringDiffA.substring(0, stringDiffB.indexOf(diffTestA)));
+				staticStrings.put(++position, stringDiffA.substring(0, stringDiffB.indexOf(diffTestA)));
+				foundMore = true;
+				break;
+			}
+			i++;
+
 		}
-		return pos;
-	}
+		if (foundMore) {
+			final Tupel<ConcurrentHashMap<Integer, String>, ConcurrentHashMap<Integer, String>> result = MagicTest.step1Helper(stringDiffA,
+					stringDiffB, ++position);
+			staticStrings.putAll(result.getObjectA());
+			variableStrings.putAll(result.getObjectB());
+		}
 
-	public static String replaceNthOccurrence(final String string, final String toFind, final int n, final String replacement) {
-		final int pos = MagicTest.nthOccurrence(string, toFind, n);
-		return (string.substring(0, pos) + string.substring(pos).replaceFirst(toFind, replacement));
+		return new Tupel<ConcurrentHashMap<Integer, String>, ConcurrentHashMap<Integer, String>>(staticStrings, variableStrings);
 	}
 
 	public MagicTest() {
 		final String[] tempArr = new String[] {
-				"The.Vampire.Diaries.S04E02.720p.HDTV.X264-DIMENSION.mkv", "The.Vampire.Diaries.S04E03.720p.HDTV.X264-DIMENSION.mkv",
-				"The.Vampire.Diaries.S04E04.720p.HDTV.X264-DIMENSION.mkv", "The.Vampire.Diaries.S04E05.720p.HDTV.X264-DIMENSION.mkv",
-				"The.Vampire.Diaries.S04E06.720p.HDTV.X264-DIMENSION.mkv", "The.Vampire.Diaries.S04E07.720p.HDTV.X264-DIMENSION.mkv",
+				"The.Vampire.Diaries.S04E02.ESTA.720p.HDTV.X264-DIMENSION.mkv",
+				"The.Vampire.Diaries.S04E03.TSTB.TESTBB.720p.HDTV.X264-DIMENSION.mkv",
+				"The.Vampire.Diaries.S04E04.TETC.720p.HDTV.X264-DIMENSION.mkv",
+				"The.Vampire.Diaries.S04E05.TESTD.TESTD.720p.HDTV.X264-DIMENSION.mkv",
+				"The.Vampire.Diaries.S04E06.TSTE.720p.HDTV.X264-DIMENSION.mkv",
+				"The.Vampire.Diaries.S04E07.TETF.720p.HDTV.X264-DIMENSION.mkv",
 		};
 
-		System.out.println(MagicTest.getSubparts(tempArr));
-		System.exit(-1);
-		final String startString = "This is my Name - S01E10 - An Example Title";
-		final String myPattern = "[STATIC] - [STATIC][VAR] - [STATIC]";
-		String testString = "This.is.my.Name.S02E08.My.Title.GERMAN.X.TEST.TEST.TEST.TEST.TEST";
-
-		final int dist = StringUtils.getLevenshteinDistance(startString, testString);
-		final int newDist = -1;
-		String diff = null;
-		int startDiff = -1;
-		final String newString = "";
-		final ConcurrentHashMap<Integer, String> matches = new ConcurrentHashMap<Integer, String>();
-		for (int i = 0; i < 10; i++) { // test only 10 iterations
-			diff = StringUtils.difference(startString, testString);
-			startDiff = testString.indexOf(diff);
-			matches.put(startDiff, testString.substring(0, startDiff));
-			System.out.println(StringUtils.difference(startString.substring(startDiff + 1), testString.substring(startDiff + 1)));
-		}
-
-		System.exit(-1);
-
-		final String s = "The red car, the red bike, and the red truck went down the red street.";
-		System.out.println(s.substring(MagicTest.nthOccurrence(s, "bike", 1)));
-		System.out.println(MagicTest.nthOccurrence(s, "bike", 1));
-		System.out.println(MagicTest.replaceNthOccurrence(s, "bike", 1, "TEEST"));
-
-		this.printDistances(startString, testString);
-
-		// 1. find separators (ignore here) (possible: "." " " "," "_" "-")
-		final String startStringSep = " ";
-		final String testStringSep = "\\.";
-
-		// 2. replace separators in test string with start string sep
-		testString = testString.replaceAll(testStringSep, startStringSep);
-
-		this.printDistances(startString, testString);
-
-		// 3. find alt separators ("-" "_")
-		final String startStringAltSep = " - ";
-		final String testStringAltSep = "";
-
-		// 4. replace each separator for one. If distance is lower, it will be
-		// saved, otherwise, it will be reversed
-		int distance = 0;
-		final int replaceCount = 1;
-		boolean doRun = true;
-		while (doRun) {
-			distance = StringUtils.getLevenshteinDistance(startString, testString);
-			System.out.println(distance);
-			for (int i = 0; i < replaceCount; i++) {
-				testString = testString.replaceFirst(startStringSep, startStringAltSep);
-			}
-			System.out.println(StringUtils.getLevenshteinDistance(startString, testString));
-			if (distance <= StringUtils.getLevenshteinDistance(startString, testString)) {
-
-			}
-			System.out.println("test");
-			doRun = false;
+		for (final String string : MagicTest.step1(tempArr)) {
+			System.out.println(string);
 		}
 
 	}
